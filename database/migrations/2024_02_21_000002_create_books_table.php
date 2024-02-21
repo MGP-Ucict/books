25f49a2ada207b11626deb6d5dcafcb338fe3b2a
MGP-Ucict <penelova.maria@gmail.com>
2024-02-21 09:38:48 +0200

    Migrations added

---
+++ database/migrations/2024_02_21_000002_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->integer('count_pages')->unsigned()->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->text('description')->nullable();
+            $table->string('file_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
