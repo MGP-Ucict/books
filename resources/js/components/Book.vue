8d2d09de75608e806e35e7fbd57f005ea0dc8f69
MGP-Ucict <penelova.maria@gmail.com>
2024-02-24 12:10:38 +0200

    Improvements

--- resources/js/components/Book.vue
+++ resources/js/components/Book.vue
@@ -32,7 +32,7 @@
             Sucessfully saved data!
         </div>
         <div class="form-group w-100">
-            <button type="submit" @click.prevent="handleSubmit"  :disabled="isDisabled && !!isEdit"class="btn btn-primary d-block mx-auto">
+            <button type="submit" @click.prevent="handleSubmit"  :disabled="isDisabled"class="btn btn-primary d-block mx-auto">
                Save
             </button>
         </div>
